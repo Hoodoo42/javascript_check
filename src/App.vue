7ec64d65f87997a31ffd5525b128f3b19994555f
Hoodoo42 <create@hoodooverse.ca>
2022-08-09 20:47:09 -0600

    added a bit more complicated data into the script section for the component. Really learning how to use what we already know about js and making it work within vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <h3>Numer of friends:{{ num_friends }}</h3>
+
     <script-example></script-example>
   </div>
 </template>
@@ -8,6 +10,33 @@
 import ScriptExample from "@/components/ScriptExample.vue";
 
 export default {
+  methods: {
+    show_info() {
+      if (this.logged_in) {
+        document.body.insertAdjacentHTML(
+          `afterbegin`,
+          `
+    <h4>You are logged in</h4>
+    `
+        );
+      } else {
+        document.body.insertAdjacentHTML(
+          `afterbegin`,
+          `
+    <h4>You are not logged in</h4>`
+        );
+      }
+
+      for (let i = 0; i < this.tweets.length; i++) {
+        document.body.insertAdjacentHTML(
+          `beforeend`,
+          `
+      <h4>${this.tweets[i]}</h4>`
+        );
+      }
+    },
+  },
+
   data() {
     return {
       tweets: [`tweet1`, `tweet2`, `tweet3`],
@@ -19,6 +48,10 @@ export default {
   name: "App",
   components: {
     ScriptExample,
+  },
+
+  mounted: function () {
+    this.show_info();
   },
 };
 </script>
